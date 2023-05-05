20346a8a4ed1c61204443fd8b3dd0aa90e5bbffb
kensteph <=kenderromain@gmail.com>
2023-05-05 18:13:01 -0400

    Fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
-    def last_3_posts
+  def last_3_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
