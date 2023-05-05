bce7bd149683525f2e4ed7731c0897da6ce92d67
kensteph <=kenderromain@gmail.com>
2023-05-05 18:02:28 -0400

    Add last 5 comments method for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def last_5_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
