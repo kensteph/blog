1eab649891429b6853f1c9d24ae2bd8c6105d4ca
kensteph <=kenderromain@gmail.com>
2023-05-05 18:03:37 -0400

    Add after_save methode for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  after_save :update_author_posts_counter
+
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_author_posts_counter
+    author.increment!(:posts_counter)
   end
 end
