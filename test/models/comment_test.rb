e45db2fcbbb6c5b1f94a4806a48f0cca085eb10c
kensteph <=kenderromain@gmail.com>
2023-05-06 02:32:20 -0400

    Add unit test for user, like, post and comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,10 @@
-require 'test_helper'
+require 'rails_helper'
 
-class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Comment, type: :model do
+  it '#update_post_comments_counter' do
+    author = User.create!(name: 'test', posts_counter: 0)
+    post = Post.create!(title: 'first post', comments_counter: 1, likes_counter: 1, author:)
+    Comment.create!(post:, author:)
+    expect(post.comments_counter).to eq(2)
+  end
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,10 @@
-require 'test_helper'
+require 'rails_helper'
 
-class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Like, type: :model do
+  it '#update_post_likes_counter' do
+    author = User.create!(name: 'test', posts_counter: 0)
+    post = Post.create!(title: 'first post', comments_counter: 1, likes_counter: 1, author:)
+    Like.create!(post:, author:)
+    expect(post.likes_counter).to eq(2)
+  end
 end
