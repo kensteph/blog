d87d70197408f8abdc9ce07d3cf248025956c00e
kensteph <=kenderromain@gmail.com>
2023-05-13 16:00:37 -0400

    configure route for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'users#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, controller: 'users/posts' do
+      resources :comments, only: [:new, :create], controller: 'users/posts/comments'
+      resources :likes, only: [:create], controller: 'users/posts/likes'
+    end
   end
 
-  root 'users#index'
 end
