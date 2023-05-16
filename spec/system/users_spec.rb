b13749f683f4854e2c9ce9529c8c2e05895f2585
Kender <kenderromain@gmail.com>
2023-05-16 12:20:17 -0400

    Write tests for : user show page.

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -15,12 +15,10 @@ RSpec.describe User, type: :system do
   describe 'index page' do
     it 'shows the rendering of user Kender name' do
       visit '/'
-      sleep(2)
       expect(page).to have_content('Kender')
     end
     it 'shows the rendering of user Nelson name' do
       visit '/'
-      sleep(2)
       expect(page).to have_content('Nelson')
     end
     it 'show the images from user Kender' do
@@ -35,28 +33,66 @@ RSpec.describe User, type: :system do
     end
     it 'show the posts_counter from user Kender' do
       visit '/'
-      sleep(2)
       expect(page.find("#counter-#{kender.id}")).to have_content kender.posts_counter.to_s
     end
     it 'show the posts_counter from user Nelson' do
       visit '/'
-      sleep(2)
       expect(page.find("#counter-#{subject.id}")).to have_content subject.posts_counter.to_s
     end
     it 'redirects from user index to user show Nelson' do
       visit '/' # Visit the user index page
-      sleep(2)
       find('span', text: subject.name).click # Click on the user's link
       expect(page).to have_current_path("/users/#{subject.id}", ignore_query: true) # Verify the redirection to user show page
     end
     it 'redirects from user index to user show Kender' do
       visit '/' # Visit the user index page
-      sleep(2)
       find('span', text: kender.name).click # Click on the user's link
       expect(page).to have_current_path("/users/#{kender.id}", ignore_query: true) # Verify the redirection to user show page
     end
   end
 
-  
+  # USER SHOW PAGE
+  describe 'User show page' do
+    it "should display the user's profile picture." do
+      visit "/users/#{subject.id}"
+      expect(page.find("img")['src']).to have_content subject.photo
+    end
+    it 'should render the username of the user' do
+      visit "/users/#{subject.id}"
+      expect(page).to have_content(subject.name)
+    end
+
+    it 'should render the  number of posts the user has written.' do
+      visit "/users/#{subject.id}"
+      expect(page.find(".post-counter")).to have_content subject.posts_counter.to_s
+    end
+
+    it 'should render the  bio of the user' do
+      visit "/users/#{subject.id}"
+      expect(page.find(".bio-text")).to have_content subject.bio.to_s
+    end
+
+    it 'should render the  3 first posts of the user' do
+      visit "/users/#{subject.id}"
+      expect(page).to have_selector('.post-comment', count: 3)
+    end
+
+    it 'should render a button that lets me view all the posts of the user' do
+      visit "/users/#{subject.id}"
+      expect(page).to have_content 'See all posts'
+    end
+
+    it 'should redirect me to the show page of that post when the user clicks on the post' do
+      visit "/users/#{subject.id}"
+      find("#show-post-#{post_1.id}").click # Click on the post show link
+      expect(page).to have_current_path("/users/#{subject.id}/posts/#{post_1.id}", ignore_query: true) # Verify the redirection to user show page
+    end
+
+    it 'should redirect me to the post index page when the user clicks on see all posts' do
+      visit "/users/#{subject.id}"
+      find(".all-posts").click # Click on the post show link
+      expect(page).to have_current_path("/users/#{subject.id}/posts", ignore_query: true) # Verify the redirection to posts index page
+    end
+  end
 
 end
