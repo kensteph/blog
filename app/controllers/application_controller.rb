9803ed3eb45d1ada995f631c638524301f8be383
Kender <kenderromain@gmail.com>
2023-05-17 18:12:48 -0400

    Add register form.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def current_user
-    @current_user ||= User.first
+  def configure_permitted_parameters
+    attributes = %i[name bio posts_counter photo]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 end
